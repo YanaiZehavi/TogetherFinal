c70d76140eb723eb3693b9df64265052d8d17626
Tal <Talolacat1@gmail.com>
2022-08-03 12:42:35 +0300

    profile page

---
+++ app/src/main/java/com/example/myapplication/travel_19_activity.java
@@ -0,0 +1,75 @@
+package com.example.myapplication;
+
+/*
+     *	This content is generated from the API File Info.
+     *	(Alt+Shift+Ctrl+I).
+     *
+     *	@desc
+     *	@file 		travel_19
+     *	@date 		Wednesday 03rd of August 2022 08:25:35 AM
+     *	@title 		Page 1
+     *	@author
+     *	@keywords
+     *	@generator 	Export Kit v1.3.figma
+     *
+     */
+
+
+
+    import android.app.Activity;
+    import android.os.Bundle;
+
+
+    import android.view.View;
+    import android.widget.TextView;
+    import android.widget.ImageView;
+
+    public class travel_19_activity extends Activity {
+
+
+        private View _bg__travel_19_ek2;
+        private TextView label;
+        private ImageView vector;
+        private ImageView vector_ek1;
+        private ImageView vector_ek2;
+        private ImageView vector_ek3;
+        private ImageView vector_ek4;
+        private ImageView vector_ek5;
+        private View oval;
+        private ImageView fill_1;
+        private ImageView fill_4;
+        private ImageView user_image;
+        private View dot;
+        private ImageView vector_ek6;
+        private ImageView logo_png__1__2;
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_travel19);
+
+
+            _bg__travel_19_ek2 = (View) findViewById(R.id._bg__travel_19_ek2);
+            label = (TextView) findViewById(R.id.label);
+            vector = (ImageView) findViewById(R.id.vector);
+            vector_ek1 = (ImageView) findViewById(R.id.vector_ek1);
+            vector_ek2 = (ImageView) findViewById(R.id.vector_ek2);
+            vector_ek3 = (ImageView) findViewById(R.id.vector_ek3);
+            vector_ek4 = (ImageView) findViewById(R.id.vector_ek4);
+            vector_ek5 = (ImageView) findViewById(R.id.vector_ek5);
+            oval = (View) findViewById(R.id.oval);
+            fill_1 = (ImageView) findViewById(R.id.fill_1);
+            fill_4 = (ImageView) findViewById(R.id.fill_4);
+            user_image = (ImageView) findViewById(R.id.user_image);
+            dot = (View) findViewById(R.id.dot);
+            vector_ek6 = (ImageView) findViewById(R.id.vector_ek6);
+            logo_png__1__2 = (ImageView) findViewById(R.id.logo_png__1__2);
+
+
+            //custom code goes here
+
+        }
+    }
+	
+	
